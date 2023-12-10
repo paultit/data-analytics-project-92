d720447afd098f078c62d6dc3c537611d2ada040
Pavlo Titarenko <titarenko.pavel@gmail.com>
2023-12-10 19:23:36 +0200

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -93,3 +93,52 @@ ORDER BY
         WHEN EXTRACT(DOW FROM sale_date) = 0 THEN 7
     END,
     EXTRACT(WEEK FROM sale_date), name;    
+    
+-- This query categorizes customers into different age groups and counts the number of customers in each group.
+SELECT
+    age_category, -- Display the age category in the result
+    COUNT(*) AS count  -- Count the number of customers in each age category
+FROM (
+    SELECT
+        CASE
+            WHEN age BETWEEN 16 AND 25 THEN '16-25'
+            WHEN age BETWEEN 26 AND 40 THEN '26-40'
+            ELSE '40+'
+        END AS age_category
+    FROM
+        customers
+) AS age_groups
+GROUP BY
+    age_category -- Group the results by age category
+ORDER BY
+    CASE
+        WHEN age_category = '16-25' THEN 1
+        WHEN age_category = '26-40' THEN 2
+        ELSE 3
+    END; -- Order the results based on the age categories
+    
+    -- This SQL query calculates the total number of unique customers and the income they generated,
+-- grouped by the date in the specified format (YYYY-MM). The results are sorted by date in ascending order.
+select 
+	to_char(sales.sale_date, 'YYYY-MM') as date,
+	COUNT(distinct sales.sales_id) as total_customers,	
+	ROUND(SUM(sales.quantity * products.price),0) as income
+	from sales join products on
+	sales.product_id = products.product_id 
+	group by date
+	order by date;
+    
+-- This SQL query retrieves information about customers who made purchases with products having a price of 0.
+-- The data includes the customer's full name, sale date, and the full name of the employee who made the sale.
+-- The results are filtered using the condition "products.price = 0," and the earliest sale date for each customer is considered.
+-- The outcome is sorted by customer_id.
+select 
+	concat(customers.first_name, ' ', customers.last_name) as customer,
+	MIN(sales.sale_date) AS sale_date,
+	concat(employees.first_name, ' ', employees.last_name) as seller
+	from sales 
+	join customers on sales.customer_id = customers.customer_id 
+	join products on sales.product_id = products.product_id 
+	join employees on sales.sales_person_id = employee_id 
+	group by customers.customer_id, employees.employee_id
+	order by customers.customer_id;    
